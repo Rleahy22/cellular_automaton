9ce7953829b6376743743562bd0cfea001848e0a
Ryan Leahy <ryanleahy90@gmail.com>
2014-02-05 17:58:17 -0600

    added updateState function to update the state of a cell

--- cell.js
+++ cell.js
@@ -1,5 +1,9 @@
 function Cell(state) {
-  this.state = state
+  this.state = state;
+
+  this.updateState = function(newState) {
+    this.state = newState;
+  }
 };
 
 module.exports = Cell
--- spec/cell-spec.js
+++ spec/cell-spec.js
@@ -1,8 +1,15 @@
 var Cell = require('../cell');
+var newCell = new Cell(0);
 
 describe("Cell", function() {
   it("has an initial state of 1 or 0", function() {
-    var newCell = new Cell(0);
     expect(newCell.state).toBe(0);
-  })
-})
+  });
+});
+
+describe("updateState", function() {
+  it("updates the state of a cell", function() {
+    newCell.updateState(1);
+    expect(newCell.state).toBe(1);
+  });
+});
