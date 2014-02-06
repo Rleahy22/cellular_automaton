2a595520d8d1d7f8e3b4def1d9d3bf88b01d6525
Ryan Leahy <ryanleahy90@gmail.com>
2014-02-05 18:27:28 -0600

    created Iteration object which contains an array of cell objects

---
+++ iteration.js
@@ -0,0 +1,6 @@
+function Iteration(cells, number) {
+  this.cells = cells;
+  this.evolutionNumber = number;
+}
+
+module.exports = Iteration;
---
+++ spec/iteration-spec.js
@@ -0,0 +1,21 @@
+var Iteration = require('../iteration');
+var Cell = require('../cell');
+
+var cells = [];
+
+for (i = 0; i < 31; i++) {
+  cell = new Cell(Math.floor(Math.random() * 2));
+  cells.push(cell);
+}
+
+var newIteration = new Iteration(cells, 1);
+
+describe("Iteration", function() {
+  it("contains an array of cell objects", function() {
+    expect(newIteration.cells[0].state).toBeDefined();
+  });
+
+  it("has an evolutionNumber that tells which iteration it is", function() {
+    expect(newIteration.evolutionNumber).toBe(1);
+  })
+})
